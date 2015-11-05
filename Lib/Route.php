9601bf7461a76c8be54f159f2562a7a5d97c125e
Jason ZOU <jason.zou@nwayschina.com>
2015-11-05 10:53:33 +0800

    路由优化

--- Lib/Route.php
+++ Lib/Route.php
@@ -25,15 +25,16 @@ class Route {
 		call_user_func_array( array(ucfirst(strtolower($arg)). '\\Controller\\'. implode('\\', $routes['controller']). 'Controller', $routes['function']. 'Action'), $routes['value']);
 	}
 	
+	/**
+	 * 分类获取路由
+	 * @return \App\Lib\array|multitype:\App\Lib\array |\App\Lib\Ambigous|\App\Lib\unknown
+	 */
 	private static function get() {
 		$key = App::config('app.url');
 		if (empty($key)) {
 			$key = 0;
 		}
 		$url = new Route();
-		
-		$url -> d();
-		
 		if (!empty(App::$request) && App::$request->isCli()) {
 			return $url->cli();	
 		}
@@ -57,56 +58,94 @@ class Route {
 			case 5:
 				return $url -> p();
 				break;
+			case 6:
+			    return $url -> d();
+			    break;
 			default:
 				return $url -> c();
 				break;
 		}
 	} 
 	
+	/**
+	 * 根据配置文件中自定义的路由解析
+	 */
 	private function d() {
 		$url    = HUrl::request_uri();
 		$url    = explode('?', $url)[0];
+		$url    = rtrim($url, '.html');
 		$url    = trim($url, '/');
 		$routes = App::config('route');
-		if (array_key_exists($url, $routes)) {
-			$file = $routes[$url];
+		foreach ($routes as $key => $value) {
+		    $pattern = str_replace(':num', '[0-9]+', $key);
+		    $pattern = str_replace(':any', '[^/]+', $pattern);
+		    $matchs  = array();
+		    preg_match($pattern, $url, $matchs);
+		    if(count($matchs) > 0) {
+		        $route = $value;
+		        foreach ($matchs as $k => $val) {
+		          $route = str_replace('$'.$k, $val, $route);
+		        }
+		        return $this->getRoute($route);
+		    }
 		}
-		
-		echo $url;
-		die();
+		return $this->getRoute($url);
 	}
 	
+	/**
+	 * 获取c、v的参数解析   ?c=home&v=index
+	 * @return multitype:array NULL
+	 */
 	private function c() {
-		return array(
-			App::$request->get('c' , 'home'),
-			App::$request->get('v' , 'index')
+		$values = explode('/', App::$request->get('v' , 'index'));
+		$routes = array(
+			'controller' => explode('/', App::$request->get('c' , 'home')),
+			'function'   => array_shift($values),
+			'value'      => $values
 		);
+		return $routes;
 	}
 	
+	/**
+	 * 获取r的参数解析     格式?r=home/index
+	 */
 	private function r() {
-		$r = App::$request->get('r', 'home/index');
-		return OString::toArray($r, '/', 2, array('home', 'index'));
+		return $this->getRoute(App::$request->get('r', 'home/index'));
 	}
 	
+	/**
+	 * 带index。php的链接解析，格式 index.php/home/index
+	 */
 	private function u() {
-		$url = HUrl::request_uri();
-		$arr = OString::toArray($url, '.php', 2, array('', '/home/index'));
-		$arr = OString::toArray($arr[1], '/', 4, array('', 'home', 'index', ''));
-		return array($arr[1], $arr[2]);
+		$url    = HUrl::request_uri();
+		$arr    = OString::toArray($url, '.php', 2, array('', '/home/index'));
+		return $this->getRoute($arr[1]);
 	}
 	
+	/**
+	 * 优雅链接解析
+	 */
 	private function y() {
-		$url = HUrl::request_uri();
-		$arr = OString::toArray($url, '/', 4, array('', 'home', 'index', ''));
-		return array($arr[1], $arr[2]);
+	    $url    = HUrl::request_uri();
+	    $url    = explode('?', $url)[0];
+		return $this->getRoute(rtrim($url, '.html'));
 	}
 	
+	/**
+	 * 自定义正则匹配路由
+	 * @return unknown
+	 */
 	private function p() {
 		$url = HUrl::request_uri();
 		preg_match($preg, $url, $result);
 		return $result;
 	}
 	
+	/**
+	 * 短链接匹配
+	 * 
+	 * @return Ambigous <array, string>
+	 */
 	private function s() {
 		$key = App::$request->get('s');
 		if ($key === null) {
@@ -126,8 +165,48 @@ class Route {
 		return $arr;
 	}
 	
+	/**
+	 * 控制台的路由
+	 */
 	private function cli() {
 		$url = App::$request->server('argv')[0];
-		return OString::toArray($url, '/', 2, array('home', 'index'));
+		return $this->getRoute($url);
+	}
+	
+	/**
+	 * 获取路由 home/index
+	 * @param string $url
+	 * @return array
+	 */
+	private function getRoute($url = 'home/index') {
+		$url    = trim($url, '/');
+		$routes = explode('/', $url);
+	    if (!isset($routes[0]) || $routes[0] === '') {
+	        $routes[0] = 'home';
+	    }
+	    if (!isset($routes[1]) || $routes[1] === '') {
+	        $routes[1] = 'index';
+	    }
+	    return $this->getValue($routes);
+	}
+	
+	/**
+	 * 根据数字判断是否带值
+	 * @param array $routes
+	 * @return array:
+	 */
+	private function getValue($routes) {
+	    $values = array();
+	    for ($i = 0, $len = count($routes); $i < $len; $i++) {
+	        if (is_numeric($routes[$i])) {
+	            $values = array_splice($routes, $i);
+	            break;
+	        }
+	    }
+	    return array(
+		    'function'   => array_pop($routes),
+		    'controller' => $routes,
+	        'value'      => $values
+		);
 	}
 }
