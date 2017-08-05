d88b4faeee56c055fdaaa31ba4b8a3021df96ba9
ZoDream <648383079@qq.com>
2017-08-05 23:52:08 +0800

    修改

--- src/Module/Gzo/Service/Controller.php
+++ src/Module/Gzo/Service/Controller.php
@@ -1,8 +1,19 @@
 <?php
 namespace Zodream\Module\Gzo\Service;
 
+use Zodream\Infrastructure\Http\Request;
 use Zodream\Service\Controller\ModuleController as BaseController;
 
 abstract class Controller extends BaseController {
-
+    protected function getActionArguments($action, $vars = array()) {
+        if (!Request::isCli()) {
+            return parent::getActionArguments($action, $vars);
+        }
+        $args = Request::argv('arguments');
+        if (empty($args)) {
+            return parent::getActionArguments($action, Request::argv('options'));
+        }
+        array_shift($args);
+        return $args;
+    }
 }
